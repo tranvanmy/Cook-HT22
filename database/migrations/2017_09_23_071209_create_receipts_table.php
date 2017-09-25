400671f438dc28ea7ce0d90fee8f9909205544f5
Son Hip <mrhip2013@gmail.com>
2017-09-25 16:12:44 +0700

    Test

---
+++ database/migrations/2017_09_23_071209_create_receipts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("user_id")->unsigned();
+            $table->string("name");
+            $table->string("description")->nullable();
+            $table->string("image");
+            $table->float("rate_point",8,2)->nullable();
+            $table->float('price', 8, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
---
+++ database/migrations/2017_09_23_074757_create_ingredients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name");
+            $table->string("description")->nullable();
+            $table->string("image")->nullable();
+            $table->integer("unit");
+            $table->integer("category_id")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
